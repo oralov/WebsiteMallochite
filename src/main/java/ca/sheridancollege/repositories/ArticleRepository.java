bdf827e20ebb0e32ceadc0895aec0ca557f42e04
Andrew Hollands <holandre@sheridancollege.ca>
2021-04-19 19:07:24 -0400

    Added ArticleRepository for CRUD operations

---
+++ src/main/java/ca/sheridancollege/repositories/ArticleRepository.java
@@ -0,0 +1,12 @@
+package ca.sheridancollege.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.sheridancollege.beans.content.Article;
+
+
+public interface ArticleRepository extends CrudRepository<Article , Integer> 
+{
+	public Article findByName(String name);
+	public Article findById(int id);
+}
