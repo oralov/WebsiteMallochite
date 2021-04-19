a5dbfb832994a3bb6f686a1dd81a4432157560e6
Andrew Hollands <holandre@sheridancollege.ca>
2021-04-19 18:59:40 -0400

    adding methods in HomeController to add , create and lookup a wiki page

--- src/main/java/ca/sheridancollege/controllers/HomeController.java
+++ src/main/java/ca/sheridancollege/controllers/HomeController.java
@@ -1,7 +1,13 @@
 package ca.sheridancollege.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ca.sheridancollege.beans.content.Article;
 
 @Controller
 public class HomeController {
@@ -9,6 +15,43 @@ public class HomeController {
 	@GetMapping("/")
 	public String goHome() {
 		return "home.html";
+	}
+	
+	
+	//	Author: Andrew Hollands
+	//	Date:	April 19th 2021
+	// gotToWiki() , addWiki() , getWikiList
+	//	This method takes an article ID as input
+	@GetMapping("/wiki/{id}")
+	public String goToWikiArticle( @PathVariable int id , Model model ) 
+	{
+		
+		
+		return "wikiArticleDisplay.html";
+	}
+	
+	@GetMapping("/wiki/")
+	public String goToWikiList( Model model ) 
+	{
+		
+		
+		return "wikiArticleDisplay.html";
+	}
+
+	@GetMapping("/wiki/add")
+	public String addWiki( Model model ) 
+	{
+		
+		
+		return "wikiArticleDisplay.html";
+	}
+	
+	@PostMapping("/wiki/add")
+	public String addWiki( Model model , @ModelAttribute Article article ) 
+	{
+		
+		
+		return "wikiArticleDisplay.html";
 	}
 
 }
